7d2edbff1ff63171aa01d12b8809a46887193e76
dineshraj100 <dineshrajmech01@gmail.com>
2022-12-15 02:30:05 +0530

    image upload config and endpoint in backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import path from 'path'
+
 import dotenv from 'dotenv'
 
 import connectDB from './config/db.js'
@@ -11,6 +13,8 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
 import orderRoutes from './routes/orderRoutes.js'
+
+import uploadRoutes from './routes/uploadRoutes.js'
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -32,9 +36,14 @@ app.use('/api/users', userRoutes)
 
 app.use('/api/orders', orderRoutes)
 
+app.use('/api/upload', uploadRoutes)
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
